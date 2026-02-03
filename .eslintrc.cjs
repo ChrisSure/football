34bd0fb0665ca0350c6d25cc1f1b475a63f36981
tkuku <taras.kukuliak@aravo.com>
2026-02-03 14:43:44 +0200

    feat:first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    es2020: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  rules: {
+    'no-console': 'off',
+    'prefer-const': 'warn',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+  },
+  ignorePatterns: ['dist', 'node_modules'],
+};
---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, { type Request, type Response } from 'express';
+
+export const app = express();
+
+app.get('/', (_req: Request, res: Response) => {
+  res.json({ status: 'ok' });
+});
+
+export const startServer = (): void => {
+  const port: number = Number(process.env.PORT ?? 3000);
+
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+};
+
+startServer();
